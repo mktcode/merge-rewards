7963be88d501b1734edbd8224058d7ab2aba3643
mktcode <kontakt@markus-kottlaender.de>
2019-06-18 01:47:37 +0200

    starting first draft

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,47 +1,52 @@
-
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        type: "text/css",
+        href:
+          "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: ["~/plugins/font-awesome"],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
+   ** Nuxt.js modules
+   */
+  modules: [],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,57 @@
 <template>
-  <div class="container"></div>
+  <div class="container d-flex pt-5">
+    <div v-if="connected" class="flex-fill">
+      <h1 class="text-center">
+        Hi <a href="https://github.com/mktcode">@mktcode</a>!
+      </h1>
+      <h3 class="text-center">Wallet: 451.68 STEEM ($309)</h3>
+      <div class="card">
+        <div class="card-header d-flex align-items-center">
+          <h5 class="m-0">Pull Requests</h5>
+          <ul class="nav nav-pills card-header-pills ml-auto">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">all</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">merged</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">open</a>
+            </li>
+          </ul>
+        </div>
+        <div class="list-group list-group-flush">
+          <div class="list-group-item d-flex align-items-center">
+            <font-awesome-icon icon="code-branch" class="merged" fixed-width />
+            <div>Cras justo odio</div>
+            <div class="ml-auto">
+              <b>Score: 84 %</b> / 1.46 STEEM
+              <a href="#" class="btn btn-sm btn-primary">Claim</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else class="flex-fill text-center">
+      <h1>Commit Tokens</h1>
+      <a class="btn btn-primary" href="#">Connect with GitHub</a>
+      <a class="btn btn-primary" href="#">Connect with Steem</a>
+      <p>Connect your Accounts to claim your rewards.</p>
+    </div>
+  </div>
 </template>
 
+<style lang="sass">
+.merged
+  color: #6f42c1
+</style>
+
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      connected: true
+    };
+  }
+};
 </script>
