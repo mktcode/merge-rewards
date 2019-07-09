7ec91a85a9d519c6645025ad729da59becbb70f8
mktcode <kontakt@markus-kottlaender.de>
2019-07-09 06:57:39 +0200

    include followers in score calculation and adjusted weights of some other factors

--- api/index.js
+++ api/index.js
@@ -43,24 +43,27 @@ const getMergedPRsLastMonth = pullRequests => {
 
 const calculateScore = (repo, user) => {
   const userAge = getAge(user.createdAt);
+  const userFollowers = user.followers.totalCount;
   const mergedPRsLastMonth = getMergedPRsLastMonth(user.pullRequests.nodes);
   const repoAge = getAge(repo.createdAt);
   const repoStars = repo.stargazers.totalCount;
   const repoForks = repo.forkCount;
   let points = 0;
 
-  if (userAge > 365) points += 1;
-  if (userAge > 365 * 5) points += 2;
-  if (mergedPRsLastMonth > 2) points += 1;
-  if (mergedPRsLastMonth > 10) points += 2;
-  if (repoAge > 90) points += 1;
-  if (repoAge > 365) points += 2;
+  if (userAge > 365) points += 2;
+  if (userAge > 365 * 5) points += 4;
+  if (userFollowers > 50) points += 1;
+  if (userFollowers > 1000) points += 2;
+  if (mergedPRsLastMonth > 2) points += 2;
+  if (mergedPRsLastMonth > 10) points += 4;
+  if (repoAge > 90) points += 2;
+  if (repoAge > 365) points += 4;
   if (repoStars > 50) points += 1;
   if (repoStars > 250) points += 2;
-  if (repoForks > 10) points += 1;
-  if (repoForks > 50) points += 2;
+  if (repoForks > 10) points += 2;
+  if (repoForks > 50) points += 4;
 
-  return Math.round((points / 15) * 100);
+  return Math.round((points / 30) * 100);
 };
 
 const getPullRequest = (pullRequest, githubAccessToken) => {
@@ -70,6 +73,9 @@ const getPullRequest = (pullRequest, githubAccessToken) => {
       query: `{
 viewer {
   createdAt,
+  followers {
+    totalCount
+  }
   pullRequests(first: 100) {
     nodes {
       mergedAt
