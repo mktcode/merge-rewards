fbf866bc666a0405991a3ce04ac26b9c12101260
mktcode <kontakt@markus-kottlaender.de>
2019-07-13 03:26:51 +0200

    added bootstrap js (including jquery and popper)

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,17 @@ export default {
         href:
           "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
       }
+    ],
+    script: [
+      { src: "https://code.jquery.com/jquery-3.3.1.slim.min.js" },
+      {
+        src:
+          "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+      },
+      {
+        src:
+          "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+      }
     ]
   },
   /*
