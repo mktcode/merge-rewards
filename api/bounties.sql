40ca5f38411bf8c2c443cd10d617080fff88deae
mktcode <kontakt@markus-kottlaender.de>
2019-07-18 13:31:07 +0200

    added schema for bounties

---
+++ api/bounties.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `bounties` (
+  `id` int(11) NOT NULL,
+  `issue` varchar(255) NOT NULL,
+  `amount` float NOT NULL,
+  `pullrequestId` varchar(64) DEFAULT NULL,
+  `claimedAt` datetime DEFAULT NULL,
+  `createdAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `bounties`
+  ADD PRIMARY KEY (`id`);
