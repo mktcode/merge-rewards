740c51c3a9f75c3a1207e18752743e6b77837cc8
mktcode <kontakt@markus-kottlaender.de>
2019-07-19 20:08:07 +0200

    changed app icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
       }
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type: "image/png", href: "/logo-with-bg.png" },
       {
         rel: "stylesheet",
         type: "text/css",
