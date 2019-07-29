b7b694ab62349564277a548a7e778c8ce26232b2
mktcode <kontakt@markus-kottlaender.de>
2019-07-29 20:42:47 +0200

    fetch bounty balance as join to deposits table

--- api/controllers/bounties.js
+++ api/controllers/bounties.js
@@ -1,7 +1,8 @@
 import database from "../database";
 import { decimalFloor } from "../../lib/helpers";
 
-const QUERY_BOUNTIES = "SELECT * FROM bounties ORDER BY createdAt ASC";
+const QUERY_BOUNTIES =
+  "SELECT b.*, SUM(bd.sbdAmount) as balance FROM bounties b JOIN bountyDeposits bd ON bd.bountyId = b.id GROUP BY b.id ORDER BY createdAt ASC";
 
 export default (req, res) => {
   database.query(QUERY_BOUNTIES, (error, result) => {
